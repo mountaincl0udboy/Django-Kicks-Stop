d61ebb384ad8386f080e9e491cd6b803ff8e4d6b
ogkkk <takemefire.99@mail.ru>
2023-04-27 21:13:40 +0600

    JWT token

--- KicksStop/urls.py
+++ KicksStop/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from sneakers.views import *
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,6 +12,8 @@ urlpatterns = [
     path('api/v1/sneaker/', SneakerAPIList.as_view()),
     path('api/v1/sneaker/<int:pk>/', SneakerAPIUpdate.as_view()),
     path('api/v1/sneakerdelete/<int:pk>/', SneakerAPIDestroy.as_view()),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
