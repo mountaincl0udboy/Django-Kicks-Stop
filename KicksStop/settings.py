50556e994e536fe72b474b35f3a2dfeff535dcb3
ogkkk <takemefire.99@mail.ru>
2023-04-27 22:19:28 +0600

    Deploying to Railway

--- KicksStop/settings.py
+++ KicksStop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-nptr#)bme3668k#++xk0ou)$h$x83mx$!sbs=e4!1hl#4dg@rr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -128,6 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn KicksStop.wsgi --log-file -
